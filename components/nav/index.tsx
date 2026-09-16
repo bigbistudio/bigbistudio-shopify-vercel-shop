9277daf608fb9a36d715e784b88e680f2dd3f6bf
ethan-nd <ethan.nd@bigbistudio.com>
2026-09-16 17:18:15 +0200

    feat(header): add logo and edit layout

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -5,6 +5,7 @@ import { Suspense } from "react";
 import { Container } from "@/components/ui/container";
 import { shopConfig } from "@/lib/config";
 import type { MenuItem } from "@/lib/shopify/transforms/menu/types";
+import { Logo } from "@/bigbistudio/components/Logo";
 
 import { NavAccount, NavAccountFallback } from "./account";
 import { CartIcon, CartIconFallback } from "./cart";
@@ -18,36 +19,42 @@ export function Nav() {
   ];
   return (
     <nav
-      className="sticky top-0 z-30 w-full bg-background pt-[env(safe-area-inset-top,0px)] transition-shadow duration-250"
+      className="sticky top-0 z-30 w-full bg-transparent pt-[env(safe-area-inset-top,0px)] transition-shadow duration-250"
       id="nav-outer"
     >
-      <Container className="flex h-16 items-center gap-2.5 md:gap-5">
-        <MobileMenu items={items} />
+      <Container className="flex justify-center py-2">
+        <div className="flex h-12 items-center gap-2.5 md:gap-5 px-2 py-2 bg-background border border-border/40 rounded-md w-full max-w-6xl">
+          <MobileMenu items={items} />
 
-        <Link className="flex items-center shrink-0" href="/">
-          <span className="text-xl leading-4">{shopConfig.site.name}</span>
-        </Link>
+          <Link className="flex items-center shrink-0" href="/">
+            <Logo
+              src="/logo.svg"
+              alt={shopConfig.site.name}
+              className="active:scale-[0.99] transition-transform duration-150"
+            />
+          </Link>
 
-        <QuickLinks items={items} />
+          <QuickLinks items={items} />
 
-        <div className="flex items-center gap-5 ml-auto">
-          {shopConfig.search.isEnabled && (
-            <PredictiveSearchProvider
-              debounceInMs={300}
-              limit={3}
-              types={["PRODUCT", "COLLECTION", "QUERY"]}
-            >
-              <SearchModal />
-            </PredictiveSearchProvider>
-          )}
-          {shopConfig.auth.isEnabled && (
-            <Suspense fallback={<NavAccountFallback />}>
-              <NavAccount />
+          <div className="flex items-center gap-5 ml-auto">
+            {shopConfig.search.isEnabled && (
+              <PredictiveSearchProvider
+                debounceInMs={300}
+                limit={3}
+                types={["PRODUCT", "COLLECTION", "QUERY"]}
+              >
+                <SearchModal />
+              </PredictiveSearchProvider>
+            )}
+            {shopConfig.auth.isEnabled && (
+              <Suspense fallback={<NavAccountFallback />}>
+                <NavAccount />
+              </Suspense>
+            )}
+            <Suspense fallback={<CartIconFallback />}>
+              <CartIcon />
             </Suspense>
-          )}
-          <Suspense fallback={<CartIconFallback />}>
-            <CartIcon />
-          </Suspense>
+          </div>
         </div>
       </Container>
     </nav>
