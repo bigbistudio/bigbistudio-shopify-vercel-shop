35450c1c7fee2df9f61bfb0d84a12f9c5c407206
ethan-nd <ethan.nd@bigbistudio.com>
2026-09-23 17:42:20 +0200

    feat(collection): update grid column gap

--- components/collections/infinite-product-grid.tsx
+++ components/collections/infinite-product-grid.tsx
@@ -73,7 +73,7 @@ export function InfiniteProductGrid<TParams>({
   }, [endCursor, hasNextPage]);
   return (
     <>
-      <div className="grid sm:grid-cols-2 gap-4 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid sm:grid-cols-2 gap-2 lg:grid-cols-3 xl:grid-cols-4">
         {children}
         {additionalProducts.map((product) => (
           <ProductCard key={product.id} product={product} outOfStockText={outOfStockText} />
