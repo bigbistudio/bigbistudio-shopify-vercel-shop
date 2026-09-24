4581f8fdb8d566453b16012dbb7f9d7088a74950
ethan-nd <ethan.nd@bigbistudio.com>
2026-09-24 18:35:42 +0200

    feat(seo): block all search engine crawlers in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,29 +1,12 @@
 import type { MetadataRoute } from "next";
 
-import { shopConfig } from "@/lib/config";
-
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
         userAgent: "*",
-        allow: "/",
-        disallow: [
-          "/cart",
-          "/account",
-          "/login",
-          "/search",
-          "/*/cart",
-          "/*/account",
-          "/*/login",
-          "/*/search",
-          "/collections/*?*sort=",
-          "/collections/*?*filter.",
-          "/*/collections/*?*sort=",
-          "/*/collections/*?*filter.",
-        ],
+        disallow: "/",
       },
     ],
-    sitemap: `${shopConfig.site.url}/sitemap.xml`,
   };
 }
