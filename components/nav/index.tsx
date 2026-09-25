c21b1989ecb0fa81a41ecafb7842c8585183484d
ethan-nd <ethan.nd@bigbistudio.com>
2026-09-25 12:37:58 +0200

    feat(nav): adjust glass effect behavior

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -25,7 +25,7 @@ export async function Nav() {
       id="nav-outer"
     >
       <Container className="flex justify-center py-2">
-        <div className="flex max-h-16 items-center justify-between gap-0.5 lg:gap-4 px-3 py-2 bg-background border border-border/40 rounded-lg w-full lg:w-fit lg:min-w-3xl lg:max-w-full overflow-hidden">
+        <div className="flex max-h-16 items-center justify-between gap-0.5 lg:gap-4 px-3 py-2 bg-background/80 lg:bg-background/45 hover:bg-background/90 duration-600 transition-colors backdrop-blur-lg lg:backdrop-blur-sm border border-border/40 rounded-lg w-full lg:w-fit lg:min-w-3xl lg:max-w-full overflow-hidden">
           <MobileMenu items={items} />
 
           <Link className="flex items-center shrink-0 flex-1 lg:flex-none" href="/">
